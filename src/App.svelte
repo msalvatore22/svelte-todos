a7ce486ae77137df246124cc771a8cb144ec654b
Michael Salvatore <msalvatore22@gmail.com>
2022-06-27 17:54:21 -0400

    Simple todo list working

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,44 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  let todos = [
+    { id: 1, done: false, description: "create todo app"},
+    { id: 2, done: false, description: "workout"},
+    { id: 3, done: false, description: "make dinner"}
+  ]
+
+  let uid = todos.length + 1;
+
+  function add(input) {
+    const todo = {
+      id: uid++,
+      done: false,
+      description: input.value
+    };
+
+    todos = [todo, ...todos];
+    input.value = '';
+  }
+
+  function remove(todo){
+    todos = todos.filter(td => td !== todo)
+  }
+  console.log(todos)
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
-</main>
+<div>
+  <input
+		placeholder="what needs to be done?"
+		on:keydown="{event => event.key === 'Enter' && add(event.target)}"
+	>
+  <ul>
+    {#each todos as todo}
+      <li>
+        {todo.description} 
+        <button on:click="{e => remove(todo)}">Delete</button>
+      </li>
+    {/each}
+  </ul>
+</div>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
 </style>
